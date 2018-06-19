dccadb3ae9b60072f0a87275e5f7de651ee789d3
Mike AUBENAS <mike.aubenas@gmail.com>
2018-06-19 23:09:26 +0200

    Now the home page (very first one) is also lazy load

--- Navigation/src/pages/home/home.ts
+++ Navigation/src/pages/home/home.ts
@@ -1,18 +1,30 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
 @Component({
 	selector: 'page-home',
-	templateUrl: 'home.html'
+	templateUrl: 'home.html',
 })
 export class HomePage
 {
-	constructor(private navCtrl: NavController)
+	constructor(private navCtrl: NavController, private navParams: NavParams)
 	{
-
 	}
 
-	navigateToSecondPage() : void
+	ionViewDidLoad()
+	{
+		console.log('ionViewDidLoad HomePage');
+	}
+
+	navigateToSecondPage(): void
 	{
 		this.navCtrl.push( 'SecondPage' );
 	}
